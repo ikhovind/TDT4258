608ace961c32c374e768aeb2934ddf5655d68dca
Ingebrigt Hovind <ing.hovind@hotmail.com>
2022-09-17 20:59:20 +0200

    hand in exercise 1

--- lab1/palin_finder.s
+++ lab1/palin_finder.s
@@ -3,133 +3,101 @@
 .section .text
 
 _start:
-	ldr r0, =input
-	bl check_input // write length to r2
-	bl to_lower
-	bl check_palindrome // check palindrome in r0 of length r2
-	// Here your execution starts
-	b exit
+	ldr r12, =input
+	bl get_length // write length to r2
+	mov r11, r3 // store length
+	bl to_lower // convert to lowercase
+	b check_palindrome // check palindrome in r12 of length r11
 
+// char at r3 is <= Z, this checks if it is >= A, if so it makes it lowercase
 possible_uppercase:
 	cmp r3, #65 // if char is after A
 	addge r3, #32 // 32 is diff between A and a
 	bx lr
 
-
-
+// converts string to lowercase
 to_lower:
- // r0 array
- // r1 counter
- // r2 length
- 	mov r1, #0
+ // r0 counter
+ 	mov r0, #0
 	push {lr} // needed because nested call
 	to_lower_loop:
-		ldrb r3, [r0, r1]
+		ldrb r3, [r12, r0]
 		cmp r3, #90 // if char is becore Z
-		blle possible_uppercase
-		strb r3, [r1, r0] // write character that might have bee updated
-		add r1, #1 // increment counter
-		cmp r1, r2 // have we reached length
+		blle possible_uppercase // update character if it really is uppercase
+		strb r3, [r12, r0] // write character that might have bee updated
+		add r0, #1 // increment counter
+		cmp r0, r11 // have we reached length
 		popeq {pc} // if so jump back
-		b to_lower_loop // if not move on
+		b to_lower_loop // if not, move on
 
-
-
-
-check_input:
-	// You could use this symbol to check for your input length
-	// you can assume that your input string is at least 2 characters
-	// long and ends with a null byte
-	mov r2, #0 // using r2 to count length
+get_length:
+	mov r3, #0 // using r3 to return length
 	loop:
-		add r2, #1 //  increment length
-		ldrb r3, [r0, r2] // use r3 to store character
-		cmp r3, #0   // is it null terminated
+		add r3, #1 //  increment length
+		ldrb r2, [r12, r3] // use r2 to store character
+		cmp r2, #0   // is it null terminated
 		bne loop      // we have not found last character
 	bx lr // back to start
 
-// checks if r0 + r1 and r0 + r2 fits our palindrome condition
-check_reg:
-	mov r3, #0 // used for result
-	ldrb r4, [r0, r1] // first char
-	ldrb r5, [r0, r2] // second char
-	cmp r4, r5 // check that they are equal
-	moveq r3, #1 // if equal res is 1
-	bx lr
+check_palindrome:
+	mov r1, r11 // count from last
+	sub r1, #1 // dont care about terminating character
+	mov r0, #0 // count from first
+	paliloop:
+		ldrb r2, [r12, r0] // beginning char
+		ldrb r3, [r12, r1] // end char
+		cmp r2, r3 // first and last char
+		bne palindrome_not_found // not eq
+		bl iterate_counters // iterate counters
+		cmp r0, r1 // have the counters crossed?
+		bge palindrome_found // if they have crossed we have checked entire word, know it's palindrome
+		b paliloop // need to check more of word
 
 // iterates register skipping over any spaces encountered
-iterate_reg:
+iterate_counters:
 	// we have to iterate at least once
-	add r1, #1
-	sub r2, #1
-	inner_iteration:
-		first_counter_loop:
-			ldrb r4, [r0, r1]
-			// if space
-			cmp r4, #32
-			// skip space
-			addeq r1, #1
-			// might be more spaces
-			beq inner_iteration
-		second_counter_loop:
-			ldrb r4, [r0, r2]
-			// if space
-			cmp r4, #32
-			// skip space
-			subeq r2, #1
-			// might be more spaces after this one
-			beq inner_iteration
+	add r0, #1
+	sub r1, #1
+	first_counter_loop:
+		ldrb r2, [r12, r0]
+		cmp r2, #32 // is char space?
+		addeq r0, #1 // skip space
+		beq first_counter_loop 	// might be more spaces
+	second_counter_loop:
+		ldrb r2, [r12, r1]
+		cmp r2, #32 // is char space
+		subeq r1, #1 // is so skip it
+		beq second_counter_loop // might be more spaces after this one
 	bx lr
-
-
-check_palindrome:
-	sub r2, #1 // dont care about terminating character
-	mov r1, #0 // this will be start counter
-	paliloop:
-		bl check_reg // check r0[r1] == r0[r2]
-		cmp r3, #1 // used as equal condtion
-		bne palindrome_not_found // not eq
-		bl iterate_reg
-		cmp r1, r2 // have the counters crossed?
-		bge palindrome_found // we have checked entire word, know it's palindrome
-		b paliloop // need to check more of word
-	bx lr
-	// Here you could check whether input is a palindrome or not
-
-write_res:
-	mov r1, #0 // used for word counter
-	ldr r6, =0xff201000 // jtag uart address
-	ldr r10, =0xFF201004
-	mov r9, #0b1000000000
-	str r9, [r10]
-
-	write_loop:
-		ldrb r3, [r0, r1] // load char
-		str r3, [r6] // write char
-		cmp r3, #0   // have we reach termination
-		beq exit    // if so exit
-		add r1, #1  // if not increment counter
-		b write_loop // repeat
 
 
 palindrome_found:
 	// Switch on only the 5 rightmost LEDs
 	// Write 'Palindrome detected' to UART
-	ldr r6, =0xff200000 // led address
-	mov r7, #0x1f // 0b0000011111
-    str r7, [r6]  // write led code
+	ldr r0, =0xff200000 // led address
+	mov r1, #0x1f // 0b0000011111 led code
+    str r1, [r0]  // write led code
 	ldr r0, =found_output // set string param
 	b write_res // write string
 
 
 palindrome_not_found:
-	ldr r6, =0xff200000 //led code
-	mov r7, #0xfe0 // 0b1111100000
-    str r7, [r6]
+	ldr r0, =0xff200000 //led code
+	mov r1, #0xfe0 // 0b1111100000
+    str r1, [r0] // write led code
 	ldr r0, =not_found_output
 	b write_res // write string
-	// Switch on only the 5 leftmost LEDs
-	// Write 'Not a palindrome' to UART
+
+write_res:
+	mov r1, #0 // used for word counter
+	ldr r2, =0xff201000 // jtag uart address
+	write_loop:
+		ldrb r3, [r0, r1] // load char
+		str r3, [r2] // write char
+		cmp r3, #0   // have we reach termination
+		beq exit    // if so exit
+		add r1, #1  // if not increment counter
+		b write_loop // repeat
 
 
 exit:
@@ -142,8 +110,7 @@ exit:
 	// This is the input you are supposed to check for a palindrome
 	// You can modify the string during development, however you
 	// are not allowed to change the label 'input'!
-	//input: .asciz "level"
-	input: .asciz "a man a plan a canal panama"
+	input: .asciz "level"
 	found_output: .asciz "Palindrome detected"
 	not_found_output: .asciz "Not a palindrome"
 	// input: .asciz "8448"
---
+++ lab2/cache_sim.c
@@ -0,0 +1,240 @@
+#include <assert.h>
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+typedef enum { dm, fa } cache_map_t;
+typedef enum { uc, sc } cache_org_t;
+typedef enum { instruction, data } access_t;
+
+typedef struct {
+  uint32_t address;
+  access_t accesstype;
+} mem_access_t;
+
+typedef struct {
+  uint64_t accesses;
+  uint64_t hits;
+  // You can declare additional statistics if
+  // you like, however you are now allowed to
+  // remove the accesses or hits
+} cache_stat_t;
+
+typedef struct {
+  unsigned int num_blocks;
+  unsigned int block_offset_bits;
+  unsigned int index_bits;
+  unsigned int num_tag_bits;
+} cache_info_t;
+
+typedef struct {
+  cache_info_t cache_info;
+  uint64_t *cache;
+} cache_t;
+
+// DECLARE CACHES AND COUNTERS FOR THE STATS HERE
+
+uint32_t cache_size;
+uint32_t block_size = 64;
+cache_map_t cache_mapping;
+cache_org_t cache_org;
+
+
+static unsigned int mylog2(unsigned int val) {
+    unsigned int ret = 0;
+    while (val > 1) {
+        val >>= 1;
+        ret++;
+    }
+    return ret;
+}
+
+// USE THIS FOR YOUR CACHE STATISTICS
+cache_stat_t cache_statistics;
+
+/* Reads a memory access from the trace file and returns
+ * 1) access type (instruction or data access
+ * 2) memory address
+ */
+mem_access_t read_transaction(FILE* ptr_file) {
+  char buf[1000];
+  char* token;
+  char* string = buf;
+  mem_access_t access;
+
+  if (fgets(buf, 1000, ptr_file) != NULL) {
+    /* Get the access type */
+    token = strsep(&string, " \n");
+    if (strcmp(token, "I") == 0) {
+      access.accesstype = instruction;
+    } else if (strcmp(token, "D") == 0) {
+      access.accesstype = data;
+    } else {
+      printf("Unkown access type\n");
+      exit(0);
+    }
+
+    /* Get the access type */
+    token = strsep(&string, " \n");
+    access.address = (uint32_t)strtol(token, NULL, 16);
+
+    return access;
+  }
+
+  /* If there are no more entries in the file,
+   * return an address 0 that will terminate the infinite loop in main
+   */
+  access.address = 0;
+  return access;
+}
+
+// gets the index of address with given cache info
+uint32_t get_index(cache_info_t cache_info, mem_access_t access) {
+  unsigned mask = (1 << cache_info.index_bits) - 1;
+  return (access.address >> cache_info.block_offset_bits) & mask;
+}
+
+uint32_t get_access_tag(cache_info_t cache_info, mem_access_t mem_access) {
+   return mem_access.address >> (cache_info.block_offset_bits + cache_info.index_bits);
+}
+
+uint32_t get_cache_tag(cache_info_t cache_info, uint32_t line_info) {
+  // remove validity bit
+  uint32_t test = line_info & ((1 << 31) - 1) ;
+  // get tag part
+  test = test >> (30 - cache_info.num_tag_bits);
+  return test;
+}
+
+bool is_valid(uint32_t line_info) {
+  return line_info & 0x80000000;
+}
+
+void replace(cache_t cache, mem_access_t access) {
+  uint32_t shifted_acc_tag = get_access_tag(cache.cache_info, access) << (31 - 1 - cache.cache_info.num_tag_bits);
+  cache.cache[get_index(cache.cache_info, access)] = shifted_acc_tag;
+  cache.cache[get_index(cache.cache_info, access)] |= 0x80000000;
+}
+
+// guess they never miss, huh...
+bool hit_or_miss(cache_t cache, mem_access_t access) {
+  uint32_t cache_line = cache.cache[get_index(cache.cache_info, access)];
+  printf("  Index is: %d\n", get_index(cache.cache_info, access));
+  printf("  access tag is: %x\n", get_access_tag(cache.cache_info, access));
+  printf("  Cache tag is: %x\n", get_cache_tag(cache.cache_info, cache.cache[get_index(cache.cache_info, access)]));
+  if (is_valid(cache_line)) {
+    if(get_access_tag(cache.cache_info, access) == get_cache_tag(cache.cache_info, cache_line)) {
+      return true;
+    }
+  }
+  replace(cache, access);
+  printf("  Cache tag after replace is: %x\n", get_cache_tag(cache.cache_info, cache.cache[get_index(cache.cache_info, access)]));
+  return false;
+}
+
+void main(int argc, char** argv) {
+  // Reset statistics:
+  memset(&cache_statistics, 0, sizeof(cache_stat_t));
+
+  /* Read command-line parameters and initialize:
+   * cache_size, cache_mapping and cache_org variables
+   */
+  /* IMPORTANT: *IF* YOU ADD COMMAND LINE PARAMETERS (you really don't need to),
+   * MAKE SURE TO ADD THEM IN THE END AND CHOOSE SENSIBLE DEFAULTS SUCH THAT WE
+   * CAN RUN THE RESULTING BINARY WITHOUT HAVING TO SUPPLY MORE PARAMETERS THAN
+   * SPECIFIED IN THE UNMODIFIED FILE (cache_size, cache_mapping and cache_org)
+   */
+  if (argc != 4) { /* argc should be 2 for correct execution */
+    printf(
+        "Usage: ./cache_sim [cache size: 128-4096] [cache mapping: dm|fa] "
+        "[cache organization: uc|sc]\n");
+    exit(0);
+  } else {
+    /* argv[0] is program name, parameters start with argv[1] */
+
+    /* Set cache size */
+    cache_size = atoi(argv[1]);
+
+
+    /* Set Cache Mapping */
+    if (strcmp(argv[2], "dm") == 0) {
+      cache_mapping = dm;
+    } else if (strcmp(argv[2], "fa") == 0) {
+      cache_mapping = fa;
+    } else {
+      printf("Unknown cache mapping\n");
+      exit(0);
+    }
+
+    /* Set Cache Organization */
+    if (strcmp(argv[3], "uc") == 0) {
+      cache_org = uc;
+    } else if (strcmp(argv[3], "sc") == 0) {
+      cache_org = sc;
+    } else {
+      printf("Unknown cache organization\n");
+      exit(0);
+    }
+  }
+
+  cache_info_t cache_info;
+  if (cache_mapping == dm && cache_org == uc) {
+    cache_info.num_blocks = cache_size / 64;
+    cache_info.block_offset_bits = 6;
+    cache_info.index_bits = mylog2(cache_info.num_blocks);
+    cache_info.num_tag_bits = 32 - cache_info.block_offset_bits - cache_info.index_bits;
+  }
+  printf("num_blocks %d\n", cache_info.num_blocks);
+  printf("block_offset_bits %d\n", cache_info.block_offset_bits);
+  printf("index_bits %d\n", cache_info.index_bits);
+  printf("num_tag_bits %d\n", cache_info.num_tag_bits);
+  cache_t cache_box;
+  cache_box.cache = calloc(cache_info.num_blocks, sizeof(uint64_t));
+  cache_box.cache_info = cache_info;
+
+  /* Open the file mem_trace.txt to read memory accesses */
+  FILE* ptr_file;
+  ptr_file = fopen("mem_trace1.txt", "r");
+  if (!ptr_file) {
+    printf("Unable to open the trace file\n");
+    exit(1);
+  }
+
+  /* Loop until whole trace file has been read */
+  mem_access_t access;
+  while (1) {
+    access = read_transaction(ptr_file);
+    // If no transactions left, break out of loop
+    if (access.address == 0) break;
+    cache_statistics.accesses++;
+    printf("%d %x\n", access.accesstype, access.address);
+    /* Do a cache access */
+    // ADD YOUR CODE HERE
+    if (cache_mapping == dm && cache_org == uc) {
+      if (hit_or_miss(cache_box, access)) {
+	printf("Cache hit");
+        cache_statistics.hits++;
+      }
+    }
+    else {
+      exit(1);
+    }
+  }
+
+  /* Print the statistics */
+  // DO NOT CHANGE THE FOLLOWING LINES!
+  printf("\nCache Statistics\n");
+  printf("-----------------\n\n");
+  printf("Accesses: %ld\n", cache_statistics.accesses);
+  printf("Hits:     %ld\n", cache_statistics.hits);
+  printf("Hit Rate: %.4f\n",
+         (double)cache_statistics.hits / cache_statistics.accesses);
+  // DO NOT CHANGE UNTIL HERE
+  // You can extend the memory statistic printing if you like!
+
+  /* Close the trace file */
+  fclose(ptr_file);
+  free(cache_box.cache);
+}
