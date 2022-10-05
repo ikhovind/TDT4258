cd856ab8ea1a3b4d06d7ceaf9693a01793cf1180
Ingebrigt Hovind <ing.hovind@hotmail.com>
2022-10-05 17:50:30 +0100

    capture joystick input

--- lab3/ex.c
+++ lab3/ex.c
@@ -2,15 +2,41 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <linux/fb.h>
+#include <linux/input.h>
 #include <sys/mman.h>
 #include <limits.h>
 #include <stdlib.h>
 #include <string.h>
+#include <poll.h>
+
+#define RED_MASK 0xF800
+#define GREEN_MASK 0x7E0
+#define BLUE_MASK 0x1F
+#define WHITE 0xFFFF
+#define DIM 8 
+
+struct input_event ev;
+
+void fill(short *fb, short len, short value) {
+  for (int i = 0; i < len; i++) {
+    fb[i] = value;
+  }
+}
+
+int positive_modulo(int x, int N) {
+  return (x % N + N) %N;
+}
 
 int main() {
   int fd = open("/dev/fb0", O_RDWR);
+  int fd2 = open("/dev/input/event0", O_RDONLY);
+  short masks[] = {RED_MASK, GREEN_MASK, BLUE_MASK};
   if (!fd) {
     printf("could not open fb0");
+    exit(1);
+  }
+  if (!fd2) {
+    printf("could not open event0");
     exit(1);
   }
   printf("Successfully opened fb0\n");
@@ -18,13 +44,33 @@ int main() {
   short *fb = (short*) mmap(0, 8 * 8 * sizeof(short), PROT_WRITE | PROT_READ, MAP_SHARED, fd, 0);
   memset(fb, 0, 8 * 8 * 2);
   close(fd);
+
+  int column = 4;
+  int row = 4;
   int i = 0;
-  while(i < 64) {
-    fb[i] = USHRT_MAX;
-    usleep(20000);
-    i++;
+  while(1) {
+    read(fd2, &ev, sizeof(struct input_event));
+    if (ev.value != 0) {
+      fb[column * 8 + row] = 0;
+      switch (ev.code) {
+        case 103:
+          column = positive_modulo(column - 1, 8);
+          break;
+        case 105:
+          row = positive_modulo(row - 1, 8);
+          break;
+        case 106:
+          row = positive_modulo(row + 1, 8);
+          break;
+        case 108:
+          column = positive_modulo(column + 1, 8);
+          break;
+      }
+      printf("column: %d, row: %d", column, row);
+      printf("index: %d\n", column * 8 + row);
+      fb[column * 8 + row] = WHITE;
+    }
   }
-  printf("loop finished\n");
 }
 
 
